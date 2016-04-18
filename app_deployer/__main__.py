94d21a64479e6a364ca2f9ddb5bd9a0aa611f663
Michael Charles <mike.charles@noaa.gov>
2016-04-18 14:15:46 -0400

    Add logging
    Start with using `logging.basicConfig()` and defining the formatting directly in `main()` for now.

--- app_deployer/__main__.py
+++ app_deployer/__main__.py
@@ -1,12 +1,19 @@
 # Import built-in packages
 import sys
+import logging
 
 # Import CPC packages
 from app_deployer import config
 
 
 def main(argv=None):
-    print("This is the main function.")
+    logging.basicConfig(
+        level=logging.DEBUG,
+        format='[%(asctime)s - %(name)s - %(levelname)s] %(message)s',
+        datefmt='%Y-%m-%d %I:%M:%S%p'
+    )
+    logger = logging.getLogger('app-deployer')
+    logger.info("This is the main function.")
 
 
 if __name__ == "__main__":
