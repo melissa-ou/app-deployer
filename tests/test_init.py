41808f71e2fc14a88c073f679c017956cca6c674
Michael Charles <mike.charles@noaa.gov>
2016-04-25 16:28:07 -0400

    Add some explicit tests
    Even though app_deployer was being implicitly tested (by the `import app_deployer` line in `test_init.py`), apparently no explicit test functions equates to a failing test on Travis CI.
    
    Now test for the `config` and `app_inventory` variables not being `None`.

--- tests/test_init.py
+++ tests/test_init.py
@@ -1,9 +1,14 @@
 import unittest
 
 # Try to import app_deployer, which will execute the functions in __init__.py
-from app_deployer import __main__
+import app_deployer
 
-if __name__ == '__main__':
-    import sys
 
-    sys.exit(unittest.main())
+def test_config_var_not_empty():
+    """Ensure the config variable isn't empty"""
+    assert app_deployer.config is not None
+
+
+def test_app_inventory_not_empty():
+    """Ensure the app inventory isn't empty"""
+    assert app_deployer.app_inventory is not None
