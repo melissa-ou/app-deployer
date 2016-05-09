858526b1712b585d26dbb833c93f451c9050be94
Mike Charles <mike.charles@noaa.gov>
2016-05-09 17:21:21 -0400

    Support custom log format depending on level
    Thanks to code found [here](http://stackoverflow.com/questions/1343227/can-pythons-logging-format-be-modified-depending-on-the-message-log-level)

--- app_deployer/logger.py
+++ app_deployer/logger.py
@@ -1,11 +1,33 @@
 # Import built-in packages
 import logging
+import sys
+
+
+class CustomFormatter(logging.Formatter):
+    default_fmt = logging.Formatter('[%(asctime)s - %(name)s - %(levelname)s] %(message)s')
+    info_fmt = logging.Formatter('%(message)s')
+
+    def __init__(self, fmt='%(message)s', datefmt='%Y-%m-%d %I:%M:%S%p'):
+        logging.Formatter.__init__(self, fmt, datefmt)
+
+    def format(self, record):
+        if record.levelno == logging.INFO:
+            return self.info_fmt.format(record)
+        else:
+            return self.default_fmt.format(record)
 
 
 def setup_logging(name):
-    logging.basicConfig(
-        level=logging.DEBUG,
-        format='[%(asctime)s - %(name)s - %(levelname)s] %(message)s',
-        datefmt='%Y-%m-%d %I:%M:%S%p'
-    )
+    fmt = CustomFormatter(datefmt='%Y-%m-%d %I:%M:%S%p')
+    handler = logging.StreamHandler(sys.stdout)
+    handler.setFormatter(fmt)
+    logging.root.addHandler(handler)
+    logging.root.setLevel(logging.INFO)
+
+    # logging.basicConfig(
+    #     level=logging.DEBUG,
+    #     format='[%(asctime)s - %(name)s - %(levelname)s] %(message)s',
+    #     datefmt='%Y-%m-%d %I:%M:%S%p'
+    # )
+
     return logging.getLogger(name)
